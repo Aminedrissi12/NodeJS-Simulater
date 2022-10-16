1b5d6eccb306aed5b6f16c949f9250bf6799a5d1
Amine Drissi <aminedrissi.1203@gmail.com>
2022-10-16 17:46:37 +0100

    i modified vairble port in the file serv

--- serv.js
+++ serv.js
@@ -1,12 +1,12 @@
 const App = require('./App')
 const dotenv = require('dotenv')
 dotenv.config()
-const porte = process.env.PORTE
+const port = process.env.PORT || 8081
 
-App.listen(porte, (err) => {
+App.listen(port, (err) => {
   if (err) {
     console.log(err)
   } else {
-    console.log(`this app is run on porte http://localhost:${porte}/`)
+    console.log(`this app is run on porte http://localhost:${port}/`)
   }
 })
